5bc9f23fd27b10c8c626e69b5c69f3ffe667d45e
Tfinch <travisfinch01@gmail.com>
2018-06-05 15:47:57 -0400

    #22 Fix

---
+++ DiscordSupportBot/Models/Explorer/BlockStats.cs
@@ -0,0 +1,24 @@
+using DiscordSupportBot.Models.BaseModels;
+using DiscordSupportBot.Modules;
+using Newtonsoft.Json;
+
+namespace DiscordSupportBot.Models.Explorer
+{
+    public class BlockStats
+    {
+        public const int currentReward = 145;
+
+        public const int firstHalveStart = 86401;
+        public const int firstHalveReward = 72;
+
+        public const int secondHalveStart = 108000;
+        public const int secondHalveReward = 36;
+
+        public const int thirdHalveStart = 129600;
+        public const int thirdHalveReward = 18;
+
+        public const int fourthHalveStart = 151201;
+        public const int fourthHalveReward = 9;
+
+    }
+}
--- DiscordSupportBot/Modules/ExplorerModule.cs
+++ DiscordSupportBot/Modules/ExplorerModule.cs
@@ -7,28 +7,92 @@ namespace DiscordSupportBot.Modules
     using System;
     using System.Collections.Generic;
     using System.Text;
+    using DiscordSupportBot.Models.Explorer;
     using System.Threading.Tasks;
     using System.Net.Http;
 
     public class ExplorerModule : ModuleBase<SocketCommandContext>
     {
         private static HttpClient client = new HttpClient();
-
+       
         [Command("stats")]
         public async Task Stats()
         {
+            int BlocksUntilHalve;
+            int CurrentBlockReward;
             var result = await this.GetStats();
 
             EmbedBuilder builder = new EmbedBuilder();
 
-            builder.WithTitle("Stats").WithColor(Discord.Color.Blue);
-            builder.AddInlineField("Difficulty", result.Difficulty);
-            builder.AddInlineField("Masternodes Count", result.MasternodeCount);
+            if (result.BlockHeight < BlockStats.firstHalveStart)
+            {
+                BlocksUntilHalve = BlockStats.firstHalveStart - result.BlockHeight;
+                CurrentBlockReward = BlockStats.currentReward;
 
+                builder.WithTitle("Stats").WithColor(Discord.Color.Blue);
+                builder.AddInlineField("Difficulty", result.Difficulty);
+                builder.AddInlineField("Masternodes Count", result.MasternodeCount);
+                builder.AddInlineField("Current Block Height", result.BlockHeight);
+                builder.AddInlineField("Blocks Until Next Halve", BlocksUntilHalve);
+                builder.AddInlineField("Current Reward Per Block", CurrentBlockReward);
+            }
+            else if (result.BlockHeight < BlockStats.secondHalveStart)
+            {
+                BlocksUntilHalve = BlockStats.secondHalveStart - result.BlockHeight;
+                CurrentBlockReward = BlockStats.firstHalveReward;
+
+                builder.WithTitle("Stats").WithColor(Discord.Color.Blue);
+                builder.AddInlineField("Difficulty", result.Difficulty);
+                builder.AddInlineField("Masternodes Count", result.MasternodeCount);
+                builder.AddInlineField("Current Block Height", result.BlockHeight);
+                builder.AddInlineField("Blocks Until Next Halve", BlocksUntilHalve);
+                builder.AddInlineField("Current Reward Per Block", CurrentBlockReward);
+            }
+            else if (result.BlockHeight < BlockStats.thirdHalveStart)
+            {
+                BlocksUntilHalve = BlockStats.thirdHalveStart - result.BlockHeight;
+                CurrentBlockReward = BlockStats.secondHalveReward;
+
+                builder.WithTitle("Stats").WithColor(Discord.Color.Blue);
+                builder.AddInlineField("Difficulty", result.Difficulty);
+                builder.AddInlineField("Masternodes Count", result.MasternodeCount);
+                builder.AddInlineField("Current Block Height", result.BlockHeight);
+                builder.AddInlineField("Blocks Until Next Halve", BlocksUntilHalve);
+                builder.AddInlineField("Current Reward Per Block", CurrentBlockReward);
+            }
+            else if (result.BlockHeight < BlockStats.fourthHalveStart)
+            {
+                BlocksUntilHalve = BlockStats.fourthHalveStart - result.BlockHeight;
+                CurrentBlockReward = BlockStats.thirdHalveReward;
+
+                builder.WithTitle("Stats").WithColor(Discord.Color.Blue);
+                builder.AddInlineField("Difficulty", result.Difficulty);
+                builder.AddInlineField("Masternodes Count", result.MasternodeCount);
+                builder.AddInlineField("Current Block Height", result.BlockHeight);
+                builder.AddInlineField("Blocks Until Next Halve", BlocksUntilHalve);
+                builder.AddInlineField("Current Reward Per Block", CurrentBlockReward);
+            }
+            else if (result.BlockHeight > BlockStats.fourthHalveStart)
+            {
+                BlocksUntilHalve = result.BlockHeight;
+                CurrentBlockReward = BlockStats.fourthHalveReward;
+
+                builder.WithTitle("Stats").WithColor(Discord.Color.Blue);
+                builder.AddInlineField("Difficulty", result.Difficulty);
+                builder.AddInlineField("Masternodes Count", result.MasternodeCount);
+                builder.AddInlineField("The Current Block Height", BlocksUntilHalve);
+                builder.AddInlineField("Current Reward Per Block", CurrentBlockReward);
+            }
+
+            
             var isBotChannel = this.Context.Channel.Id.Equals(DiscordSupportBot.Common.DiscordData.BotChannel);
 
-            await this.Context.Guild.GetTextChannel(DiscordSupportBot.Common.DiscordData.BotChannel)
-                .SendMessageAsync(isBotChannel ? string.Empty : this.Context.Message.Author.Mention, false, builder.Build());
+
+            await this.ReplyAsync(string.Empty, false, builder.Build());
+           // var isBotChannel = this.Context.Channel.Id.Equals(DiscordSupportBot.Common.DiscordData.BotChannel);
+
+           // await this.Context.Guild.GetTextChannel(DiscordSupportBot.Common.DiscordData.BotChannel)
+              //  .SendMessageAsync(isBotChannel ? string.Empty : this.Context.Message.Author.Mention, false, builder.Build());
         }
 
         [Command("balance")]
@@ -57,10 +121,12 @@ namespace DiscordSupportBot.Modules
             var difficultyResponse = await client.GetAsync($"https://explorer.ipsum.network/api/getdifficulty");
             var masternodeResponse = await client.GetAsync($"https://explorer.ipsum.network/api/getmasternodecount");
             var supplyResponse = await client.GetAsync($"https://explorer.ipsum.network/api/getsupply");
+            var blockResponse = await client.GetAsync($"https://explorer.ipsum.network/api/getblockcount");
 
             var result = new ExplorerStatsResponse
             {
                 Difficulty = float.Parse(difficultyResponse.Content.ReadAsStringAsync().Result),
+                BlockHeight = int.Parse(blockResponse.Content.ReadAsStringAsync().Result),
                 MasternodeCount = int.Parse(JsonConvert.DeserializeObject<dynamic>(masternodeResponse.Content.ReadAsStringAsync().Result).total.Value.ToString())
             };
 
@@ -71,12 +137,14 @@ namespace DiscordSupportBot.Modules
         {
             public float Difficulty { get; set; }
             public int MasternodeCount { get; set; }
+            public int BlockHeight { get; set; }
         }
 
         public enum StatsDataType
         {
             Difficulty,
-            MasternodeCount
+            MasternodeCount,
+            BlockHeight
         }
     }
 }
